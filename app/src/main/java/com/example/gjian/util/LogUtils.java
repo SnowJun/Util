9ac2c99163354af63363912f7440f7d583a98f4f
Gjian <872851894@qq.com>
2016-07-06 16:33:52 +0800

    fix some bug and other

--- app/src/main/java/com/example/gjian/util/LogUtils.java
+++ app/src/main/java/com/example/gjian/util/LogUtils.java
@@ -3,12 +3,21 @@ package com.example.gjian.util;
 import android.util.Log;
 
 /**
+ * Log的工具类，Log管理
+ * 根据级别关闭，调试模式关闭
  * Created by Administrator on 2016/7/4 0004.
  */
 public class LogUtils {
 
+    /**
+     * 调试模式
+     */
     private static final boolean DEBUG = true;
-
+    /**
+     * Log级别
+    * 默认为最低级别，即全部打印
+    */
+    private int logLevel = Log.VERBOSE;
 
     private static LogUtils ourInstance;
     public static LogUtils getInstance() {
@@ -21,29 +30,74 @@ public class LogUtils {
     private LogUtils() {
     }
 
-    public void setIsLogVerbose(){
-
+    /**
+     * V1.0
+     * 打印V级别Log
+     * @param tag Log的TAG值
+     * @param log Log内容
+     */
+    public void v(String tag,String log){
+        if (DEBUG && logLevel >= Log.VERBOSE){
+            Log.v(tag,"log_util_v:"+log);
+        }
     }
 
-    public void setIsLogDebug(){
-
+    /**
+     * V1.0
+     * 打印D级别Log
+     * @param tag Log的TAG值
+     * @param log Log内容
+     */
+    public void d(String tag,String log){
+        if (DEBUG && logLevel >= Log.DEBUG){
+            Log.d(tag,"log_util_v:"+log);
+        }
     }
 
-    public void setIsLogInfo(){
-
+    /**
+     * V1.0
+     * 打印I级别Log
+     * @param tag Log的TAG值
+     * @param log Log内容
+     */
+    public void i(String tag,String log){
+        if (DEBUG && logLevel >= Log.INFO){
+            Log.i(tag,"log_util_v:"+log);
+        }
     }
 
-    public void setIsLogWarn(){
-
+    /**
+     * V1.0
+     * 打印W级别Log
+     * @param tag Log的TAG值
+     * @param log Log内容
+     */
+    public void w(String tag,String log){
+        if (DEBUG && logLevel >= Log.WARN){
+            Log.w(tag,"log_util_v:"+log);
+        }
     }
 
-    public void setIsLogError(){
-
+    /**
+     * V1.0
+     * 打印E级别Log
+     * @param tag Log的TAG值
+     * @param log Log内容
+     */
+    public void e(String tag,String log){
+        if (DEBUG && logLevel >= Log.ERROR){
+            Log.e(tag,"log_util_v:"+log);
+        }
     }
 
 
-    public boolean isShowLog(int level){
-        return true;
+    /**
+     * 设置Log的打印级别
+     * @param level Log的打印级别
+     * 取值为Log.VERBOSE到Log.ERROR
+     */
+    public void setLogLevel(int level){
+       logLevel = level;
     }
 
 
--- app/src/main/java/com/example/gjian/util/NextActivity.java
+++ app/src/main/java/com/example/gjian/util/NextActivity.java
@@ -2,15 +2,19 @@ package com.example.gjian.util;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by Administrator on 2016/6/21.
  */
 public class NextActivity extends Activity {
 
+    private RecyclerView mRvDemo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycle_view);
         ToastUtils.getInstance(getApplicationContext()).showToast("next page",false);
     }
 }
