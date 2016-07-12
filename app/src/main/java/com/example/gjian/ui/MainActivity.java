399e4734970949a11ce2b71015d0657dda3da0b2
Gjian <872851894@qq.com>
2016-07-12 19:04:27 +0800

    snackbar 使用

--- app/src/main/java/com/example/gjian/ui/MainActivity.java
+++ app/src/main/java/com/example/gjian/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gjian.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private TextView mTxtImgCenter;
     private TextView mTxtCancel;
     private TextView mTxtNext;
+    private TextView mTxtSnack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +42,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mTxtImgCenter = (TextView) findViewById(R.id.txt_img_center);
         mTxtCancel = (TextView) findViewById(R.id.txt_cancel);
         mTxtNext = (TextView) findViewById(R.id.txt_next);
+        mTxtSnack = (TextView) findViewById(R.id.txt_snack);
         mTxtNormal.setOnClickListener(this);
         mTxtNormalCenter.setOnClickListener(this);
         mTxtImg.setOnClickListener(this);
         mTxtImgCenter.setOnClickListener(this);
         mTxtCancel.setOnClickListener(this);
         mTxtNext.setOnClickListener(this);
+        mTxtSnack.setOnClickListener(this);
     }
 
     @Override
@@ -62,6 +67,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             case R.id.txt_img_center:
                 ToastUtils.getInstance(getApplicationContext()).showToastWithImage("image center toast",false,R.mipmap.ic_launcher,Gravity.CENTER);
                 break;
+            case R.id.txt_snack:
+                showSnackBar();
+                break;
             case R.id.txt_cancel:
                 ToastUtils.getInstance(this).cancelToast();
                 break;
@@ -71,6 +79,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             default:
                 break;
         }
+    }
+
+    private void showSnackBar() {
+        final Snackbar snackbar = Snackbar.make(mTxtSnack,"SnackBar",Snackbar.LENGTH_LONG);
+        snackbar.setAction("取消", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.getView().setBackgroundColor(Color.GREEN);
+        snackbar.setActionTextColor(Color.RED);
+        snackbar.show();
     }
 
 
